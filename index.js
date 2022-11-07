b74318e9ce71a57e98279b883a418603c6c03688
Kadyu <100702835+Kadyu@users.noreply.github.com>
2022-11-07 18:49:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('keydown', function() {
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage ="images/" + "dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage ="images/" + "dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage)
+
+    if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "PLayer 1 wins";
+    }
+    else if (randomNumber1 < randomNumber2){
+        document.querySelector("h1").innerHTML = "PLayer 2 wins";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw";
+    }
+
+});
